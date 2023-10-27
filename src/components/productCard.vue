e98a2d3f16426c6c62e850ecd3748fcc069d53f6
Tomas Holgado <creadordecaminos@gmail.com>
2023-10-26 21:51:54 -0300

    Added some style to product cards (animation included)

--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -8,9 +8,26 @@
 
 <style scoped>
     .productCard{
+        height: 90%;
+        background-color: white;
         display: flex;
         flex-direction: column;
+        /* justify-content: center; */
         margin: 5% 10%;
+        border: 1px rgba(0, 0, 0, 0.3) solid;
+        padding: 5px 10px;
+        border-radius: 10px;
+        align-items: center;
+        transform: scale(1.03);
+        transition: .2s;
+    }
+    .productCard:hover {
+        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+        transform: scale(1.05);
+    }
+    
+    .productCard input {
+        width: 80%;
     }
 </style>
 
