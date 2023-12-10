78d42c07e290422ca453440711f295fcdb7871ce
Tomas Francisco Holgado <creadordecaminos@live.com.ar>
2023-12-10 20:30:10 -0300

    CSS de la página Home ajustado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 
 <template>
   <main class="landingPage">
-    <div class="firstHalf">.</div>
+    <div class="firstHalf"></div>
     <div class="content">
       <div class="contentImg">
         <img src="../assets/img/raquetas.jpg" alt="raquetas landing page">
@@ -18,11 +18,11 @@
         <RouterLink to="/products">Ver más</RouterLink>
       </div>
     </div>
-      <div class="secondHalf"></div>
   </main>
 </template>
 
 <style scoped>
+
   .landingPage {
     width: 100%;
     /* background-color: gray; */
@@ -32,7 +32,7 @@
   }
   .firstHalf {
     background-color: #5ced46;
-    width: 70%;
+    width: 100%;
     color: #5ced46;
   }
   .content {
@@ -68,7 +68,6 @@
     font-size: 21px;
     border-radius: 20px;
     padding: 20px 15px;
-    width: 20%;
     margin: 0 auto;
     cursor: pointer;
     transition: .25s;
@@ -78,4 +77,86 @@
   .contentText a:hover {
     background-color: rgb(50, 50, 50);
   }
+  
+@media (max-width: 1024px) {
+  .landingPage {
+    width: 100%;
+    /* background-color: gray; */
+    flex: 1;
+    display: flex;
+    position: relative;
+  }
+
+  .firstHalf {
+    display: none;
+    background-color: #5ced46;
+    width: 100%;
+    color: #5ced46;
+    position: absolute;
+    height: 100%;
+  }
+
+  .content {
+    position: static;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    padding-top: 20px;
+    width: 100%; /* Cambiado a 100% para adaptarse horizontalmente */
+    max-width: 1200px; /* Ajusta el ancho máximo según tus necesidades */
+    margin: 0 auto; /* Centra el contenido horizontalmente */
+    background-color: #5ced46;
+  }
+
+  .contentImg {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+
+  .contentImg img {
+    width: 80%;
+    max-width: 100%; /* Ajuste para que la imagen no se achique demasiado */
+  }
+
+  .contentText {
+    padding: 2rem; /* Añadido un padding general */
+    width: 80%; /* Cambiado a 100% para adaptarse horizontalmente */
+    text-align: center; /* Centrado horizontalmente */
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Centrado verticalmente */
+  }
+
+  .contentText h1 {
+    font: 700;
+    font-size: 3em;
+    text-decoration: underline;
+  }
+
+  .content p {
+    font: 500;
+    font-size: 1.75em;
+  }
+
+  .contentText a {
+    background-color: black;
+    color: white;
+    font: bold;
+    font-size: 21px;
+    border-radius: 20px;
+    padding: 20px 15px;
+    width: 70%; /* Cambiado a 70% para adaptarse horizontalmente */
+    max-width: 300px; /* Ajusta el ancho máximo según tus necesidades */
+    margin-top: 20px; /* Añadido margen superior */
+    cursor: pointer;
+    transition: .25s;
+    text-decoration: none;
+    text-align: center;
+  }
+
+  .contentText a:hover {
+    background-color: rgb(50, 50, 50);
+  }
+}
 </style>
